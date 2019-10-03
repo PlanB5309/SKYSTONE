5c0166ea77692af81b7553f7833529ccf80964f7
Dillonrobotics <dpwa1k3r@gmail.com>
2019-10-03 19:36:37 -0400

    Created an initial TeleOp and Hardware program.

---
+++ TeamCode/src/main/java/RobotHardware.java
@@ -0,0 +1,97 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left front drive motor:        "leftFrontDrive"
+ * Motor channel:  Right front drive motor:        "rightFrontDrive"
+ * Motor channel:  Left rear drive motor:        "leftRearDrive"
+ * Motor channel:  Right rear drive motor:        "rightRearDrive"
+ */
+public class RobotHardware
+{
+    /* Public OpMode members. */
+    public DcMotor  leftFrontDrive   = null;
+    public DcMotor  rightFrontDrive  = null;
+    public DcMotor  leftRearDrive   = null;
+    public DcMotor  rightRearDrive  = null;
+
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+
+    //Hardware constants
+    public double TELEOPDEADZONE = 0.05;
+    /* Constructor */
+    public RobotHardware(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftFrontDrive  = hwMap.get(DcMotor.class, "leftFrontDrive");
+        rightFrontDrive = hwMap.get(DcMotor.class, "rightFrontDrive");
+        leftRearDrive  = hwMap.get(DcMotor.class, "leftFrontDrive");
+        rightRearDrive = hwMap.get(DcMotor.class, "rightFrontDrive");
+
+
+        // Set all motors to zero power
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftRearDrive.setPower(0);
+        rightRearDrive.setPower(0);
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftRearDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightRearDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Define and initialize ALL installed servos.
+
+    }
+ }
+
---
+++ TeamCode/src/main/java/TeleOp.java
@@ -0,0 +1,108 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+/**
+
+ */
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Teleop", group="Teleop")
+
+public class TeleOp extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    RobotHardware robot           = new RobotHardware();   // Use a Pushbot's hardware
+
+
+    @Override
+    public void runOpMode() {
+        double drive;
+        double turn;
+        double sideways;
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Run wheels in POV mode (note: The joystick goes negative when pushed forwards, so negate it)
+            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            // This way it's also easy to just drive straight, or just turn.
+            drive = -gamepad1.left_stick_y;
+            sideways  =  gamepad1.left_stick_x;
+            turn  =  gamepad1.right_stick_x;
+            if (Math.abs(drive) > robot.TELEOPDEADZONE)
+            {
+                robot.leftFrontDrive.setPower(Range.clip(drive, -1.0, 1.0));
+                robot.rightFrontDrive.setPower(Range.clip(drive, -1.0, 1.0));
+                robot.leftRearDrive.setPower(Range.clip(drive, -1.0, 1.0));
+                robot.rightRearDrive.setPower(Range.clip(drive, -1.0, 1.0));
+            }
+            else if (Math.abs(sideways) > robot.TELEOPDEADZONE)
+            {
+                robot.leftFrontDrive.setPower(Range.clip(-sideways, -1.0, 1.0));
+                robot.rightFrontDrive.setPower(Range.clip(-sideways, -1.0, 1.0));
+                robot.leftRearDrive.setPower(Range.clip(sideways, -1.0, 1.0));
+                robot.rightRearDrive.setPower(Range.clip(sideways, -1.0, 1.0));
+            }
+            else if (Math.abs(turn) > robot.TELEOPDEADZONE)
+            {
+                robot.leftFrontDrive.setPower(Range.clip(-turn, -1.0, 1.0));
+                robot.rightFrontDrive.setPower(Range.clip(turn, -1.0, 1.0));
+                robot.leftRearDrive.setPower(Range.clip(-turn, -1.0, 1.0));
+                robot.rightRearDrive.setPower(Range.clip(turn, -1.0, 1.0));
+            }
+            // Send telemetry message to signify robot running;
+            telemetry.addData("leftFrontDrive",  "Offset = %.2f", robot.leftFrontDrive.getPower());
+            telemetry.addData("rightFrontDrive",  "%.2f", robot.rightFrontDrive.getPower());
+            telemetry.addData("leftRearDrive", "%.2f", robot.leftRearDrive.getPower());
+            telemetry.addData("rightRearDrive", "%.2f", robot.rightRearDrive.getPower());
+
+            telemetry.update();
+
+            // Pace this loop so jaw action is reasonable speed.
+            sleep(50);
+        }
+    }
+}
