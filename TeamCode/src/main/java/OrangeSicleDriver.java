6f2a69811fdedaf128e3c29b7a70ae8ae5f6c080
dylanswist <swistdylan@gmail.com>
2019-10-13 15:03:26 -0400

    Added a program for test drive.

---
+++ TeamCode/src/main/java/OrangeSicleDriver.java
@@ -0,0 +1,20 @@
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="OrangeSicleDriver")
+public class OrangeSicleDriver extends LinearOpMode {
+    RobotHardware robot           = new RobotHardware();   // Use a Pushbot's hardware
+    LinearOpMode linearOpMode= new LinearOpMode() {
+        @Override
+        public void runOpMode() throws InterruptedException {
+
+        }
+    };
+        Drive drive = new Drive(robot,telemetry, linearOpMode);
+    public void runOpMode() throws InterruptedException{
+        robot.init(hardwareMap);
+        waitForStart();
+        drive.forward(0.5,12);
+    }
+}
