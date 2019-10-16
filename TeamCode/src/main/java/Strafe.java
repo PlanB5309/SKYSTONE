64f4cee00d075043a8246ed1b9cdbcb841808182
AlexaStubblefield <queenofnewyork03@gmail.com>
2019-10-16 19:41:37 -0400

    Added strafe code

---
+++ TeamCode/src/main/java/Strafe.java
@@ -0,0 +1,89 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+public class Strafe {
+    RobotHardware robot;
+    Telemetry telemetry;
+    LinearOpMode linearOpMode;
+
+    public Strafe(RobotHardware robot, Telemetry telemetry, LinearOpMode linearOpMode) {
+        this.robot = robot;
+        this.telemetry = telemetry;
+        this.linearOpMode = linearOpMode;
+    }
+
+    public void left(double speed, int distance) throws InterruptedException {
+        robot.leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftRearDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightRearDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftRearDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightRearDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        int target = distance * robot.CLICKS_PER_INCH;
+        robot.leftFrontDrive.setTargetPosition(-target);
+        robot.leftRearDrive.setTargetPosition(target);
+        robot.rightFrontDrive.setTargetPosition(target);
+        robot.rightRearDrive.setTargetPosition(-target);
+
+        robot.leftFrontDrive.setPower(-speed);
+        robot.leftRearDrive.setPower(speed);
+        robot.rightFrontDrive.setPower(speed);
+        robot.rightRearDrive.setPower(-speed);
+
+        telemetry.addData("linear opmode is working, target = ", target);
+//       Thread.sleep(5000);
+//        telemetry.addData("", target);
+        while (robot.leftRearDrive.isBusy()) {
+            Thread.yield();
+            telemetry.addData("Encoder Clicks", robot.leftRearDrive.getCurrentPosition());
+        }
+
+        robot.leftFrontDrive.setPower(0);
+        robot.leftRearDrive.setPower(0);
+        robot.rightFrontDrive.setPower(0);
+        robot.leftRearDrive.setPower(0);
+
+    }
+
+    public void right(double speed, int distance) {
+        robot.leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftRearDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightRearDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftRearDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightRearDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        int target = distance * robot.CLICKS_PER_INCH;
+        robot.leftFrontDrive.setTargetPosition(target);
+        robot.leftRearDrive.setTargetPosition(-target);
+        robot.rightFrontDrive.setTargetPosition(-target);
+        robot.rightRearDrive.setTargetPosition(target);
+
+        robot.leftFrontDrive.setPower(speed);
+        robot.leftRearDrive.setPower(-speed);
+        robot.rightFrontDrive.setPower(-speed);
+        robot.rightRearDrive.setPower(speed);
+
+        telemetry.addData("linear opmode is working, target = ", target);
+//       Thread.sleep(5000);
+//        telemetry.addData("", target);
+        while (robot.leftRearDrive.isBusy()) {
+            Thread.yield();
+            telemetry.addData("Encoder Clicks", robot.leftRearDrive.getCurrentPosition());
+        }
+
+        robot.leftFrontDrive.setPower(0);
+        robot.leftRearDrive.setPower(0);
+        robot.rightFrontDrive.setPower(0);
+        robot.leftRearDrive.setPower(0);
+
+    }
+}
