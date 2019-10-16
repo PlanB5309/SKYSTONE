c7a397477f970728539b40aefdc03e3c35a10eca
jessicF <email@email.email>
2019-10-16 19:37:01 -0400

    Adding more code to test the new Strafe class.

--- TeamCode/src/main/java/OrangeSicleDriver.java
+++ TeamCode/src/main/java/OrangeSicleDriver.java
@@ -13,10 +13,13 @@ public class OrangeSicleDriver extends LinearOpMode {
     };
 
     Drive drive = new Drive(robot, telemetry, linearOpMode);
+    Strafe strafe = new Strafe(robot, telemetry, linearOpMode);
     public void runOpMode() throws InterruptedException{
         robot.init(hardwareMap);
         waitForStart();
         drive.forward(0.2,36);
+        strafe.right(0.2, 36);
         drive.backward(0.2,36);
+        strafe.left(0.2, 36);
     }
 }
