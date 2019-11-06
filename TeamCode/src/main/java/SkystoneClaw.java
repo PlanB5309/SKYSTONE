f16af91b0d85cabaad86222a0b3237a05ae81e25
parkerduggins <parkerduggins@gmail.com>
2019-11-05 19:23:41 -0500

    Made new class for the skystone claw.

---
+++ TeamCode/src/main/java/SkystoneClaw.java
@@ -0,0 +1,25 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class SkyStoneClaw {
+
+    RobotHardware robot;
+    Telemetry telemetry;
+    LinearOpMode linearOpMode;
+
+    public SkyStoneClaw (RobotHardware robot, Telemetry telemetry, LinearOpMode linearOpMode){
+        this.robot = robot;
+        this.telemetry = telemetry;
+        this.linearOpMode = linearOpMode;
+    }
+    public void down () throws InterruptedException {
+        robot.skyStoneClaw.setPosition(robot.SKYSTONE_SERVO_DOWN);
+        Thread.sleep (500);
+    }
+    public void up () throws InterruptedException {
+        robot.skyStoneClaw.setPosition(robot.SKYSTONE_SERVO_UP);
+        Thread.sleep(500);
+    }
+
+}
