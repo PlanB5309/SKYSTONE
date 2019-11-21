36a02abfe2caf2294d080b415934bddf1bd7776e
DillonWGithub <dpwa1k3r@gmail.com>
2019-11-20 19:53:29 -0500

    Added Strafe to StopOnLine

--- TeamCode/src/main/java/StopOnLine.java
+++ TeamCode/src/main/java/StopOnLine.java
@@ -93,6 +93,86 @@ public class StopOnLine {
         robot.leftRearDrive.setPower(0);
 
     }
+
+    public void strafeRight(double speed, int distance) {
+        setupDriveTrain();
+
+        int target = distance * robot.STRAFE_CLICKS_PER_INCH;
+        robot.leftFrontDrive.setTargetPosition(target);
+        robot.leftRearDrive.setTargetPosition(-target);
+        robot.rightFrontDrive.setTargetPosition(-target);
+        robot.rightRearDrive.setTargetPosition(target);
+
+        robot.leftFrontDrive.setPower(speed);
+        robot.leftRearDrive.setPower(-speed);
+        robot.rightFrontDrive.setPower(-speed);
+        robot.rightRearDrive.setPower(speed);
+
+        int red = robot.colorSensor.red();
+        int blue  = robot.colorSensor.blue();
+
+        telemetry.addData("linear opmode is working, target = ", target);
+        telemetry.addData("Encoder Clicks", robot.leftRearDrive.getCurrentPosition());
+
+//       Thread.sleep(5000);
+//        telemetry.addData("", target);
+        while (robot.leftRearDrive.isBusy() && linearOpMode.opModeIsActive() && red < robot.RED_THRESHOLD && blue < robot.BLUE_THRESHOLD)  {
+
+            blue  = robot.colorSensor.blue();
+            red = robot.colorSensor.red();
+            Thread.yield();
+            telemetry.addData("Encoder Clicks", robot.leftRearDrive.getCurrentPosition());
+            telemetry.addData("blue value", blue);
+            telemetry.addData("red value", red);
+            telemetry.update();
+        }
+
+        robot.leftFrontDrive.setPower(0);
+        robot.leftRearDrive.setPower(0);
+        robot.rightFrontDrive.setPower(0);
+        robot.leftRearDrive.setPower(0);
+
+    }
+
+    public void strafeLeft(double speed, int distance) {
+        setupDriveTrain();
+
+        int target = distance * robot.STRAFE_CLICKS_PER_INCH;
+        robot.leftFrontDrive.setTargetPosition(-target);
+        robot.leftRearDrive.setTargetPosition(target);
+        robot.rightFrontDrive.setTargetPosition(target);
+        robot.rightRearDrive.setTargetPosition(-target);
+
+        robot.leftFrontDrive.setPower(-speed);
+        robot.leftRearDrive.setPower(speed);
+        robot.rightFrontDrive.setPower(speed);
+        robot.rightRearDrive.setPower(-speed);
+
+        int red = robot.colorSensor.red();
+        int blue  = robot.colorSensor.blue();
+
+        telemetry.addData("linear opmode is working, target = ", target);
+        telemetry.addData("Encoder Clicks", robot.leftRearDrive.getCurrentPosition());
+
+//       Thread.sleep(5000);
+//        telemetry.addData("", target);
+        while (robot.leftRearDrive.isBusy() && linearOpMode.opModeIsActive() && red < robot.RED_THRESHOLD && blue < robot.BLUE_THRESHOLD)  {
+
+            blue  = robot.colorSensor.blue();
+            red = robot.colorSensor.red();
+            Thread.yield();
+            telemetry.addData("Encoder Clicks", robot.leftRearDrive.getCurrentPosition());
+            telemetry.addData("blue value", blue);
+            telemetry.addData("red value", red);
+            telemetry.update();
+        }
+
+        robot.leftFrontDrive.setPower(0);
+        robot.leftRearDrive.setPower(0);
+        robot.rightFrontDrive.setPower(0);
+        robot.leftRearDrive.setPower(0);
+
+    }
     private void setupDriveTrain () {
         robot.leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.leftRearDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
