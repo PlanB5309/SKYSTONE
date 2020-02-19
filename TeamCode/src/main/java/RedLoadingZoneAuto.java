60146e53634881f50cc95c2abf3bbc0766c19742
parkerduggins <parkerduggins@gmail.com>
2020-02-19 16:08:42 -0500

    Did position 1 for red loading zone autonomous.

--- TeamCode/src/main/java/RedLoadingZoneAuto.java
+++ TeamCode/src/main/java/RedLoadingZoneAuto.java
@@ -27,7 +27,55 @@ public class RedLoadingZoneAuto extends LinearOpMode {
         blockNum = findSkyStone.instant(AllianceColor.Red);
 
         if(blockNum == 1){
+
+            //go to the first skystone
             stopAtDistance.left(0.15, robot.blockDistance[blockNum], 20);
+
+            //get the block and take it to the tray
+            blockGrabber.grab();
+            robot.blockFlippingServo.setPosition(robot.LIFT_BLOCK_SERVO_UP);
+            drive.backward(0.25, 6);
+            gyroTurn.absolute(0);
+            strafe.right (0.5, 80);
+
+            //Put the block on the tray
+            gyroTurn.absolute(0);
+            drive.forward(0.3,7);
+            blockArm.down();
+            robot.blockGrabbingServo.setPosition(robot.BLOCK_SERVO_RELEASE);
+
+            //Grab the tray and spin it
+            foundationClaws.down();
+            robot.blockFlippingServo.setPosition(robot.LIFT_BLOCK_SERVO_UP);
+            drive.backward(0.3,18);
+            gyroTurn.absolute(-90);
+            robot.rightFoundationServo.setPosition(robot.RIGHT_FOUNDATION_SERVO_UP);
+            robot.leftFoundationServo.setPosition(robot.LEFT_FOUNDATION_SERVO_UP);
+            drive.forward(0.3,10);
+
+            //Go get the second Skystone
+            stopAtDistance.right(0.3,65,10);
+            gyroTurn.absolute(-90);
+            robot.blockFlippingServo.setPosition(robot.LIFT_BLOCK_SERVO_DOWN);
+            drive.backward(0.5,62 + 8 * 3);
+            gyroTurn.absolute(0);
+
+            //position for next skystone
+            stopAtDistance.left(0.25,robot.blockDistance [4],10);
+            stopAtDistance.forward(0.1, 5, 12);
+
+            //grab the second skystone
+            blockGrabber.grab();
+            robot.blockFlippingServo.setPosition(robot.LIFT_BLOCK_BARLEY_UP);
+            drive.backward(0.25, 6);
+            gyroTurn.absolute(-90);
+            drive.forward(0.5, 67);
+
+            //drop the block and park
+            robot.blockGrabbingServo.setPosition(robot.BLOCK_SERVO_RELEASE);
+            gyroTurn.absolute(-92);
+            stopOnLine.backward(0.4,20);
+
         }
         if(blockNum == 2) {
             blockGrabber.grab();
@@ -71,7 +119,7 @@ public class RedLoadingZoneAuto extends LinearOpMode {
 
             //drop the block and park
             robot.blockGrabbingServo.setPosition(robot.BLOCK_SERVO_RELEASE);
-            gyroTurn.absolute(-90);
+            gyroTurn.absolute(-92);
             stopOnLine.backward(0.4,20);
         }
         if(blockNum == 3) {
